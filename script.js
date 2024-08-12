2456c4515618ecc390d7ab10dee0e47fc9b728ee
ambutsa <145941214+ambutsa@users.noreply.github.com>
2024-08-12 12:17:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const giftCard = document.querySelector('.gift-card');
+
+// Handle click and touch events
 giftCard.addEventListener('click', () => {
+    giftCard.classList.toggle('flipped');
+});
+
+giftCard.addEventListener('touchstart', () => {
     giftCard.classList.toggle('flipped');
 });
